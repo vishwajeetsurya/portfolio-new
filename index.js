017639ff01f671ef474ebe291dc7bf36ed05432f
Vishwaa <vish@gmail.com>
2024-09-27 13:14:05 +0530

    dist add

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ require("dotenv").config({ path: "./.env" })   // env File Path
 
 const app = express()
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : "https://portfolio-new-92bp.onrender.com   ", credentials: true }))
 app.use(cookieParser())
 app.use(express.json())
 
+app.use(express.static("dist"))
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
 app.use("/api/public", require("./routes/public.routes"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resours Not found" })
+
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 app.use((err, req, res, next) => {
     console.log(err)
