d5e286bd9d7218cda547d7e150a1a241c3aa3bc6
Vishwaa <vish@gmail.com>
2024-09-30 12:49:17 +0530

    path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const { adminProtected } = require("./middleware/Protected")
 require("dotenv").config({ path: "./.env" })   // env File Path 
-
+const path = require("path")
 const app = express()
 // Middleware
 app.use(cors({ origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : "https://portfolio-new-92bp.onrender.com   ", credentials: true }))
@@ -17,7 +17,6 @@ app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
 app.use("/api/public", require("./routes/public.routes"))
 
 app.use("*", (req, res) => {
-
     res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 app.use((err, req, res, next) => {
